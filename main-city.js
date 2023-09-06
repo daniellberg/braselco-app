7680462a300326e45c085c073b1c76f47b1bb738
Daniel Berg <daniel.berg@bsc.com>
2023-09-06 10:37:33 -0300

    braselco app

---
+++ main-city.js
@@ -0,0 +1,26 @@
+const key = "fa44fade42e33bd2078692e794c4c387"
+
+function dataInsert(data) {
+
+    console.log(data)
+    document.querySelector('.weather-city').innerHTML = data.name;
+    document.querySelector('.tempp').innerHTML = data.main.temp.toFixed(1)
+    document.querySelector('.feelslike').innerHTML = data.main.feels_like.toFixed(1)
+    document.querySelector('.weather-category').innerHTML = data.weather[0].description
+    document.querySelector('.umidd').innerHTML = data.main.humidity.toFixed(1)
+    document.querySelector('.press').innerHTML = data.main.pressure
+    document.querySelector('.speed').innerHTML = data.wind.speed
+    document.querySelector('.img-forecast').src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+}
+
+async function citySearch(cityName) {
+    const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${key}&lang=pt_br&units=metric`).then(response => response.json());
+    dataInsert(data)
+}
+
+function buttonClick() {
+    const cityName = document.querySelector('.input-city').value;
+    citySearch(cityName)
+    document.querySelector('.input-city').value = '';
+
+}
